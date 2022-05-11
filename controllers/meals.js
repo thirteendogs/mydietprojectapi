8bd3fbd11e62ba2cd2fb41a54a96f4b937c533fd
Daniel <daniel.nova.girbau@gmail.com>
2022-05-11 02:25:30 +0200

    Add login api

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const mealsRouter = require('express').Router()
 const Meal = require('../models/meal')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 mealsRouter.get('/', async (request, response) => {
   const meals = await Meal
@@ -20,10 +22,22 @@ mealsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 mealsRouter.post('/', async (request, response) => {
   const body = request.body
-  console.log("bodyyy", body);
-  const user = await User.findById(body.user)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const meal = new Meal({
     ingredients: body.ingredients,
